716be33c35c7bda39ea312e3d7b7479ab658661e
ethanpasta <ethan.mayer9@gmail.com>
2019-02-07 01:06:42 +0000

    Change command to write instead of fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,8 +8,8 @@
  */
 int main(void)
 {
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-fprintf(stderr, "%s\n", str);
+write(2, str, 60);
 return (1);
 }
