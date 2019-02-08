970b620f6852daec035c3a152605d0f76b6d89e1
ethanpasta <ethan.mayer9@gmail.com>
2019-02-08 02:16:53 +0000

    Change loop type to while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,36 +7,25 @@
  */
 int main(void)
 {
-	int n1, n2, n3, n4;
 	int num1 = 0, num2 = 0;
 
-	for (n1 = 0; n1 <= 9; n1++)
-	{
-		for (n2 = 0; n2 <= 9; n2++)
+	for (num1 = 0; num1 <= 99; num1++)
+		for (num2 = 0; num2 <= 99; num2++)
 		{
-			for (n3 = 0; n3 <= 9; n3++)
+			if (num1 < num2)
 			{
-				for (n4 = 0; n4 <= 9; n4++)
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98)
 				{
-					num1 = n1 * 10 + n2;
-					num2 = n3 * 10 + n4;
-					if (num1 < num2)
-					{
-						putchar(n1 + 48);
-						putchar(n2 + 48);
-						putchar(' ');
-						putchar(n3 + 48);
-						putchar(n4 + 48);
-						if (num1 != 98)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }
