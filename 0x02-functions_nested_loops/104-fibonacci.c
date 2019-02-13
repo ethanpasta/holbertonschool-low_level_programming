633d0974dfc3df2c26411c973dd12ed4afb3f301
ethanpasta <ethan.mayer9@gmail.com>
2019-02-13 23:59:48 +0000

    Add function description

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: exit of main function
+ */
 int main(void)
 {
 	int i;
 	long a = 1, b = 2, c;
 	long n11, n12, n21, n22, sum1, sum2;
 	long split = 1000000000;
+
 	printf("%lu, %lu", a, b);
 	for (i = 0; i < 88; i++)
 	{
@@ -14,14 +20,14 @@ int main(void)
 		a = b;
 		b = c;
 	}
-	n11 = a / 1000000000;
-	n12 = a % 1000000000;
-	n21 = b / 1000000000;
-	n22 = b % 1000000000;
+	n11 = a / split;
+	n12 = a % split;
+	n21 = b / split;
+	n22 = b % split;
 	for (i = 1; i <= 7; i++)
 	{
-		sum1 = n11 + n21;
-		sum2 = n12 + n22;
+		sum1 = n11 + n21 + ((n12 + n22) / split);
+		sum2 = (n12 + n22) % split;
 		printf(", %lu%lu", sum1, sum2);
 		n11 = n21;
 		n12 = n22;
