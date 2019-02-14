1dc897e72533fe6a191154809a5ac838347557cc
ethanpasta <ethan.mayer9@gmail.com>
2019-02-14 23:48:03 +0000

    Remove trailing white space

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	int i;
-
+	
+	printf("1");
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 	return (0);
