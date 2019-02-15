b5fc080532c46952205cac1c60ff2b5b33fa0b66
ethanpasta <ethan.mayer9@gmail.com>
2019-02-15 01:04:30 +0000

    Change algorithm type

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-/**
- * prime - Function checks if a number is prime
- * @num: Number to check
- *
- * Return: 1 if prime, 0 otherwise
- */
-int prime(int num)
-{
-	double root = sqrt((double)num);
-	int check = 1, i;
-
-	for (i = 2; i < root; i++)
-		if (num % i == 0)
-			check = 0;
-	return (check);
-}
 
 /**
  * main - entry point
@@ -24,15 +8,17 @@ int prime(int num)
 int main(void)
 {
 	long num = 612852475143;
-	double root = sqrt((double)num);
 	int big = 0;
 	int i;
 
-	root = (int)root;
-	for (i = 2; i <= root; i++)
-		if ((num % i == 0) && prime(i))
-			if (i > big)
-				big = i;
+	for (i = 3; i <= sqrt(num); i++)
+	{
+		while (num % i == 0)
+		{
+			big = i;
+			num = num / i;
+		}
+	}
 	printf("%d\n", big);
 	return (0);
 }
