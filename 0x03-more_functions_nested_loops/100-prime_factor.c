e4f0e734a2c8e98c597fe912b4b99bada4d06080
ethanpasta <ethan.mayer9@gmail.com>
2019-02-15 02:35:51 +0000

    New version

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -7,18 +7,19 @@
  */
 int main(void)
 {
-	long num = 612852475143;
-	int big = 0;
+	long n = 612852475143;
+	int maxPrime = -1;
 	int i;
 
-	for (i = 3; i <= sqrt(num); i++)
+	for (i = 3; i <= sqrt(n); i += 2)
 	{
-		while (num % i == 0)
-		{
-			big = i;
-			num = num / i;
+		while (n % i == 0) {
+			maxPrime = i;
+			n = n / i;
 		}
 	}
-	printf("%d\n", big);
+	if (n > 2)
+		maxPrime = n;
+	printf("%d\n", maxPrime);
 	return (0);
 }
