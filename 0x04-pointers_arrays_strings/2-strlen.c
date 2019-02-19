981421c2af93fc3f157243165d9402ecac169775
ethanpasta <ethan.mayer9@gmail.com>
2019-02-19 19:49:01 +0000

    Typo in function parameter name

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,15 @@
 
 /**
  * _strlen - returns length of a string
- * @c: string to check
+ * @s: string to check
  *
  * Return: string length
  */
 int _strlen(char *s)
 {
-	int i = 0;
-	int count = 0;
+	int count = -1;
 
-	while (s[count++] != 0)
+	while (s[++count] != 0)
 		;
 	return (count);
 }
