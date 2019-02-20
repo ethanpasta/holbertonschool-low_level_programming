e17d5dcd27ba76e257211f4bc8a41ea51c77c4e6
ethanpasta <ethan.mayer9@gmail.com>
2019-02-20 04:52:50 +0000

    Added function description

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * _atoi - function converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: Integer converted
+ */
 int _atoi(char *s)
 {
 	int num = 0, i = -1, sign = 1;
