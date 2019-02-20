b462322846bdc9e7566e5658e435af08d8933b53
ethanpasta <ethan.mayer9@gmail.com>
2019-02-20 05:38:29 +0000

    typo

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,23 @@
  */
 void puts_half(char *str)
 {
-	int size = 0, n;
+	int size = 0;
+	int n;
 
-	while (str[size] != '\0')
-		size++;
-	if (size % 2 == 1)
-		n = (size - 1) / 2;
-	else
+	for (; str[size] != '\0'; size++)
+	{}
+	if (size % 2 == 0)
+	{
 		n = size / 2;
+	}
+	else
+	{
+		n = (size + 1) / 2;
+	}
+
 	for (; str[n] != '\0'; n++)
+	{
 		_putchar(str[n]);
+	}
 	_putchar('\n');
 }
