352f7d1225849c08e114a93ba723d1385f36aad8
ethanpasta <ethan.mayer9@gmail.com>
2019-02-21 21:36:08 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * cap_string - function capitalizes all words
+ * of a string
+ * @s: string to change
+ *
+ * Return: new string
+ */
+char *cap_string(char *s)
+{
+	int i = 0, check = 0, j;
+	char ch[14] = {' ', '\n', '\t', ',', ';', '!'
+		       , '?', '"', '(', ')', '{', '}'};
+
+	while (s[i] != '\0')
+	{
+		check = 0;
+		for (j = 0; j < 13; j++)
+			if (s[i] == ch[j])
+				check = 1;
+		if (check)
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+				s[i + 1] -= 32;
+		i++;
+	}
+	return (s);
+}
