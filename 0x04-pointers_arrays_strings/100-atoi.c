39876a8a68946e0c182dd01dfcffd62985976f6e
ethanpasta <ethan.mayer9@gmail.com>
2019-02-21 00:37:48 +0000

    Change for max negative int

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int num = 0, i = -1, sign = 1;
+	int i = 0, sign = 1, num = 0;
 
-	while ((s[++i] < '0' || s[i] > '9') && s[i])
+	while ((s[i] < '0' || s[i] > '9') && s[i])
 	{
 		if (s[i] == '-')
-			sign *= -1;
-	}
-	if (s[i])
-	{
-		num = s[i++] - '0';
-		num *= sign;
-		while (s[i] >= '0' && s[i] <= '9')
 		{
-			num *= 10;
-			num += (s[i] - '0');
-			i++;
+			sign = sign * (-1);
+		}
+		i++;
+	}
+	if (s[i] != '\0')
+	{
+		if (sign == -1)
+		{
+			num = (s[i] - '0') * sign;
+			i = i + 1;
+			while (s[i] <= '9' && s[i] >= '0')
+			{
+				num = num * 10;
+				num = num - (s[i] - '0');
+				i++;
+			}
+		}
+		else
+		{
+			num = s[i] - '0';
+			i = i + 1;
+			while (s[i] <= '9' && s[i] >= '0')
+			{
+				num = num * 10;
+				num = num + (s[i] - '0');
+				i++;
+			}
 		}
 	}
 	return (num);
