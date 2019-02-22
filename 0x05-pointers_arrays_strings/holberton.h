a3002e934b555b02a60e60400a13e27a365ab3ba
ethanpasta <ethan.mayer9@gmail.com>
2019-02-22 01:04:22 +0000

    Update to header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,11 @@
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
+int _putchar(char);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
