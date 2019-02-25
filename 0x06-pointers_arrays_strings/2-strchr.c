6086c2793926777b020479319259eddbc6acb671
ethanpasta <ethan.mayer9@gmail.com>
2019-02-25 23:00:55 +0000

    temp variable to not ruin string

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	char *s1 = s;
+
+	while (*s1)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (*s1 == c)
+			return (s1);
+		s1++;
 	}
 	return (NULL);
 }
