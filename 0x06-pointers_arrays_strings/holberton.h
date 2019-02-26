451e0d19d07f66be08db3bada13e69e1812ca9f7
ethanpasta <ethan.mayer9@gmail.com>
2019-02-26 00:51:09 +0000

    Header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,4 @@
+#define NULL 0
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -7,4 +8,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+void set_string(char **s, char *t);
