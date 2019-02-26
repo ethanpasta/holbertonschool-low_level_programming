daf3866eddb3033ada724a1eb81ab1c95ad5b621
ethanpasta <ethan.mayer9@gmail.com>
2019-02-26 00:37:32 +0000

    Change method

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
+		if (*s == c)
+			return (s);
+		s++;
 	}
 	return (NULL);
 }
