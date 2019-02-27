225d494a314fc51e435c64d7cd08354519294843
ethanpasta <ethan.mayer9@gmail.com>
2019-02-27 21:58:54 +0000

    Betty warnings

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-        if (*s)
-                return (_strlen_recursion(s + 1) + 1);
-        else
-                return (0);
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+	else
+		return (0);
 }
 
 /**
