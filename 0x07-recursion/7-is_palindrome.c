af22a8991781a075326533d24ab4e47c8f4cd5e8
ethanpasta <ethan.mayer9@gmail.com>
2019-02-28 05:44:58 +0000

    Again

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,10 +1,23 @@
 #include "holberton.h"
 
 /**
- * check_palindrome - function checks if a string is a palindrome
- * @beg: beginning position
- * @end: last position
- * @s: string to use
+ * lenS - function returns string length
+ * @s: string to check
+ *
+ * Return: string length
+ */
+int lenS(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (lenS(s + 1) + 1);
+}
+
+/**
+ * check_palindrome - function checks if string is palindrome
+ * @s: string to check
+ * @beg: first position
+ * @end: second position
  *
  * Return: 1 if palindrome, 0 otherwise
  */
@@ -12,22 +25,22 @@ int check_palindrome(char *s, int beg, int end)
 {
 	if (beg == end || beg + 1 == end)
 		return (1);
-	else if (s[beg] != s[end])
+	if (s[beg] != s[end])
 		return (0);
 	return (check_palindrome(s, beg + 1, end - 1));
 }
 
 /**
- * is_palindrome - function checks if string is a palindrome
+ * is_palindrome - function checks if string is palindrome
  * @s: string to check
  *
  * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	if (s == '\0')
 		return (1);
-	int len = _strlen_recursion(s) - 1;
+	int end = lenS(s) - 1;
 
-	return (check_palindrome(s, 0, len));
+	return (check_palindrome(s, 0, end));
 }
