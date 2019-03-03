886032cd0f3f62495fdf68fa31bd96d826f3c902
ethanpasta <ethan.mayer9@gmail.com>
2019-03-03 20:59:55 +0000

    another try

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,36 +1,55 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - entry point
- * @argc: number of arguments
- * @argv: array of parameters
- *
- * Return: exit value
+ * returnCents - calculates the min number of coins needed to make change given
+ * @n: target change value
+ * Return: min num of coins
  */
-int main(int argc, char *argv[])
+int returnCents(int n)
 {
+	int coins = 0;
+
+	while (n)
+	{
+		if (n >= 25)
+			n -= 25;
+		else if (n >= 10)
+			n -= 10;
+		else if (n >= 5)
+			n -= 5;
+		else if (n >= 2)
+			n -= 2;
+		else if (n >= 1)
+			n -= 1;
+		coins++;
+	}
+	return (coins);
+}
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
+ */
+int main(int argc, char **argv)
+{
+	int number;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cents = 0, change = atoi(argv[1]);
-
-	while (change > 0)
+	number = atoi(argv[1]);
+	if (number < 0)
 	{
-		if (change >= 25)
-			change -= 25;
-		else if (change >= 10)
-			change -= 10;
-		else if (change >= 5)
-			change -= 5;
-		else if (change >= 2)
-			change -= 2;
-		else if (change >= 1)
-			change -= 1;
-		cents++;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", cents);
+	printf("%i\n", returnCents(number));
 	return (0);
 }
