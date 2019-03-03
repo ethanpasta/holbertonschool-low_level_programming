ba2bce46cc6dea71e6e913bf84de1f8ad03d4e97
ethanpasta <ethan.mayer9@gmail.com>
2019-03-03 19:51:08 +0000

    Advanced task

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of parameters
+ *
+ * Return: exit value
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int cents = 0, change = atoi(argv[1]);
+
+	if (change < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (change >= 25)
+	{
+		cents += 1;
+		change -= 25;
+	}
+	while (change >= 10)
+	{
+		cents += 1;
+		change -= 10;
+	}
+	while (change >= 5)
+	{
+		cents += 1;
+		change -= 5;
+	}
+	while (change >= 2)
+	{
+		cents += 1;
+		change -= 2;
+	}
+	if (change != 0)
+		cents += 1;
+	printf("%d\n", cents);
+	return (0);
+}
