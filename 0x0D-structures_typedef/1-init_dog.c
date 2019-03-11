aefac9446464a22d8679b2596e864cf371dcfe46
ethanpasta <ethan.mayer9@gmail.com>
2019-03-11 22:09:18 +0000

    Added edge case if struct is null

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -12,7 +12,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		if (name)
+			d->name = name;
+		d->age = age;
+		if (owner)
+			d->owner = owner;
+	}
 }
