a5912df67d8cf1e2adc3c41394690fc4f8f4f108
ethanpasta <ethan.mayer9@gmail.com>
2019-03-11 23:03:35 +0000

    Seperated free statemts

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -32,9 +32,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!name || !owner || !dog)
 		return (NULL);
 	name1 = malloc(_strlen(name) + 1);
-	owner1 = malloc(_strlen(owner) + 1);
-	if (!name1 || !owner1)
+	if (!name1)
 	{
+		free(dog);
+		return (NULL);
+	}
+	owner1 = malloc(_strlen(owner) + 1);
+	if (!owner1)
+	{
+		free(name1);
 		free(dog);
 		return (NULL);
 	}
