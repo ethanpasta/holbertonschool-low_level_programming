18510ac473ffb4c9eb4d0b20acdbcc4463159db9
ethanpasta <ethan.mayer9@gmail.com>
2019-03-11 23:26:38 +0000

    agin

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -1,19 +1,10 @@
-#include "dog.h"
-#include <stdlib.h>
-
-/**
- * free_dog - function frees dogs
- * @d: dog structure
- *
- * Return: none
- */
 void free_dog(dog_t *d)
 {
-	if (!d)
-		return;
-	if (d->name)
-		free(d->name);
-	if (d->owner)
-		free(d->owner);
-	free(d);
+        if (!d)
+                return;
+        if (d->name)
+                free(d->name);
+        if (d->owner)
+                free(d->owner);
+        free(d);
 }
