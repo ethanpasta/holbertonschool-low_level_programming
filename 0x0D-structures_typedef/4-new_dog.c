b8d2ed99b1f128ddbb1af01b6ccbd593eca32c19
ethanpasta <ethan.mayer9@gmail.com>
2019-03-11 22:55:48 +0000

    Using strlen

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,19 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - function returns length of string
+ * @s: string to check
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	if (*s)
+		return (_strlen(s + 1) + 1);
+	return (0);
+}
 
 /**
  * new_dog - function creates a new struct of type dog
@@ -7,17 +21,31 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: struct of dog
+ * Return: pointer to struct of dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name1 = name, *owner1 = owner;
-	dog_t *tmp = malloc(sizeof(dog_t));
+	int i;
+	char *name1, *owner1;
+	dog_t *dog = malloc(sizeof(dog_t));
 
-	if (!tmp)
+	if (!name || !owner || !dog)
 		return (NULL);
-	tmp->name = name1;
-	tmp->owner = owner1;
-	tmp->age = age;
-	return (tmp);
+	name1 = malloc(_strlen(name));
+	owner1 = malloc(_strlen(owner));
+	if (!name1 || !owner1)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		name1[i] = name[i];
+	name1[i] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
+		owner1[i] = owner[i];
+	owner1[i] = '\0';
+	dog->name = name1;
+	dog->owner = owner1;
+	dog->age = age;
+	return (dog);
 }
