425d26a591f6e4cc78b49f34f6cfe23877e4cd1b
ethanpasta <ethan.mayer9@gmail.com>
2019-03-13 21:49:28 +0000

    Check if parameters are null

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -16,12 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	while (i < size)
+	if (array && cmp)
 	{
-		check = cmp(array[i]);
-		if (check)
-			return (i);
-		i++;
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
 	}
 	return (-1);
 }
