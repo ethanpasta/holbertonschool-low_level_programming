51a670e88d48b999dadd6769dd91deb12a4d883f
ethanpasta <ethan.mayer9@gmail.com>
2019-03-14 01:06:42 +0000

    advanced task

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,21 @@
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of arguments
+ *
+ * Return: exit status
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
