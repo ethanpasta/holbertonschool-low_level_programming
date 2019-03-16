6ae24aef69c3baa789d74b7df5e6bd64711f5cde
ethanpasta <ethan.mayer9@gmail.com>
2019-03-16 02:46:23 +0000

    Header file

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#include <stdarg.h>
+
+typedef struct type_st
+{
+	char c;
+	void (*f)(va_list);
+} type_st;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
