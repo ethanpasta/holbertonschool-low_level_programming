e582054252b80c5beb10adc349e61e57e7f1243b
ethanpasta <ethan.mayer9@gmail.com>
2019-05-05 05:24:37 +0000

    Edge case for first position

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * inset_dnodeint_at_index - function inserts a new node at a given position
+ * insert_dnodeint_at_index - function inserts a new node at a given position
  * @h: head of list
  * @idx: index where the new node should be added
  * @n: value of new node
@@ -13,14 +13,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h, *new;
 	unsigned int i;
 
-	for (i = 0; tmp && i < idx - 1; i++)
-		tmp = tmp->next;
-	if (!tmp)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	for (i = 0; tmp && i < idx - 1; i++)
+		tmp = tmp->next;
+	if (!tmp)
+		return (NULL);
 	new->next = tmp->next;
 	new->prev = tmp;
 	tmp->next = new;
