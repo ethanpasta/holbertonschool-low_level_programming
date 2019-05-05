6137e5dd47653b7e2c31921c6c6c19ecef2789cc
ethanpasta <ethan.mayer9@gmail.com>
2019-05-05 03:50:31 +0000

    Task 0

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,20 @@
-first
+#include "lists.h"
+
+/**
+ * print_dlistint - function prints all the elements of a
+ * dlistint_t list
+ * @h: head of list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len;
+
+	for (len = 0; h; len++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (len);
+}
