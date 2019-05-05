2615fba97830d9eb99c87b6b7161bb3eaafd2fc7
ethanpasta <ethan.mayer9@gmail.com>
2019-05-05 05:16:40 +0000

    Take care of prev links

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -16,8 +16,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
+	if (!*head)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 	new->next = *head;
-	new->prev = NULL;
+	(*head)->prev = new;
 	*head = new;
 	return (new);
 }
