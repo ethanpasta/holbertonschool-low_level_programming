5ff5afe324907259b5ccd58f89db08aa3b757fbd
ethanpasta <ethan.mayer9@gmail.com>
2019-05-06 22:58:51 +0000

    Change wget link

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/ethanpasta/holbertonschool-low_level_programming/blob/master/0x17-dynamic_libraries/inject.so
+wget https://github.com/ethanpasta/holbertonschool-low_level_programming/raw/master/tmp/inject.so
 export LD_PRELOAD=$PWD/inject.so
