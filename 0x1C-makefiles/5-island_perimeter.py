8d106f684d65089c53a03080bd4b6c33020bb8ad
ethanpasta <ethan.mayer9@gmail.com>
2019-07-12 00:33:45 +0000

    Add edge grid checks

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,15 +9,15 @@ def island_perimeter(grid):
        grid: grid
     """
     perims = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i <= 0 or grid[i - 1][j] == 0:
                     perims += 1
-                if grid[i + 1][j] == 0:
+                if i >= len(grid) or grid[i + 1][j] == 0:
                     perims += 1
-                if grid[i][j + 1] == 0:
+                if j <= 0 or grid[i][j + 1] == 0:
                     perims += 1
-                if grid[i][j - 1] == 0:
+                if j >= len(grid[i]) or grid[i][j - 1] == 0:
                     perims += 1
     return perims
