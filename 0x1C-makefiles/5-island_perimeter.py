5ae2b24ff5ee526f0dbbf9aaf6439f85f5857996
ethanpasta <ethan.mayer9@gmail.com>
2019-07-12 00:15:34 +0000

    Change for loops to sum

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,18 +8,5 @@ def island_perimeter(grid):
     Args:
        grid: grid
     """
-    perims = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                num_ones = 0
-                if i > 0:
-                    num_ones += grid[i - 1][j]
-                if i < len(grid):
-                    num_ones += grid[i + 1][j]
-                if j > 0:
-                    num_ones += grid[i][j + 1]
-                if j < len(grid[i]):
-                    num_ones += grid[i][j - 1]
-                perims += (4 - num_ones)
-    return perims
+    perims = sum([sum(grid[i]) for i in range(len(grid))])
+    return ((perims * 2) + 2) if perims != 0 else 0
