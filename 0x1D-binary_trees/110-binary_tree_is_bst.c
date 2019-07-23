a2aa8cb4c86027223e4485ce702900ce903be202
ethanpasta <ethan.mayer9@gmail.com>
2019-07-23 21:40:20 +0000

    Advanced task 24

---
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * every_node_different - function checks if every node in tree
+ * is bigger/smaller than certain value
+ * @tree: pointer to root
+ * @value: value to compare to
+ * @diff: 1 if comparison is greater, 0 if smaller
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int every_node_different(binary_tree_t *tree, int value, int diff)
+{
+	if (!tree)
+		return (1);
+	if (diff && tree->n <= value)
+		return (0);
+	else if (!diff && tree->n >= value)
+		return (0);
+	return (every_node_different(tree->left, value, diff)
+		&& every_node_different(tree->right, value, diff));
+}
+
+/**
+ * binary_tree_is_bst - function checks if a binary tree is a
+ * valid binary search tree
+ * @tree: pointer to the root
+ *
+ * Return: 1 if valid bst, 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+	if ((tree->right && tree->right->n <= tree->n) ||
+	    !every_node_different(tree->right, tree->n, 1))
+		return (0);
+	if ((tree->left && tree->left->n >= tree->n) ||
+	    !every_node_different(tree->left, tree->n, 0))
+		return (0);
+	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
+		return (0);
+	return (1);
+}
