6d593c10158ec0c9f21f6ec56cc33c1872d3b99b
ethanpasta <ethan.mayer9@gmail.com>
2019-07-25 11:07:40 +0000

    Add helper function

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,14 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - function measures the depth of a node in a binary tree
+ * binary_tree_depth_help - function measures the depth of a
+ * node in a binary tree
  * @tree: a pointer to the to measure the depth
  *
  * Return: depth of the node
  */
+size_t binary_tree_depth_help(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_depth_help(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_depth - function measures the depth of a node in a binary tree
+ * @tree: a pointer to the root
+ *
+ * Return: the depth
+ */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (-1);
-	return (binary_tree_depth(tree->parent) + 1);
+		return (0);
+	return (binary_tree_depth_help(tree) - 1);
 }
