e647f2dc54208d01fe66a6b28cba604b9c236283
ethanpasta <ethan.mayer9@gmail.com>
2019-07-25 11:04:13 +0000

    Add helper function

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,20 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - function measures the height of a binary tree
+ * binary_tree_height_help - function measures the height of a binary tree
  * @tree: a pointer to the root
  *
  * Return: height of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_help(const binary_tree_t *tree)
 {
-	int max_left, max_right;
+	int max_left, max_right, max;
 
 	if (!tree)
-		return (-1);
-	max_left = binary_tree_height(tree->left);
-	max_right = binary_tree_height(tree->right);
+		return (0);
+	max_left = binary_tree_height_help(tree->left);
+	max_right = binary_tree_height_help(tree->right);
 	if (max_left > max_right)
-		return (max_left + 1);
-	return (max_right + 1);
+		max = max_left;
+	else
+		max = max_right;
+	return (max + 1);
+}
+
+/**
+ * binary_tree_height - function measures the height of a binary tree
+ * @tree: a pointer to the root
+ *
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height_help(tree) - 1);
 }
