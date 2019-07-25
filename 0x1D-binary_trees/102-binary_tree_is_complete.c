6156285efb8873dbfe7ecc4483451bff2ffe3597
ethanpasta <ethan.mayer9@gmail.com>
2019-07-25 18:38:42 +0000

    Change index recursion value

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -10,7 +10,27 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_is_complete_help - function helper
+ * @tree: pointer to root
+ * @index: index of current node
+ * @size: number of nodes in tree
+ *
+ * Return: 1 if complete, 0 otherwise
+ */
+int binary_tree_is_complete_help(const binary_tree_t *tree,
+				 int index, int size)
+{
+	if (!tree)
+		return (1);
+	if (index >= size)
+		return (0);
+	return (binary_tree_is_complete_help(tree->left, 2 * index + 1, size) &&
+		binary_tree_is_complete_help(tree->right, 2 * index + 2, size));
 }
 
 /**
@@ -21,40 +41,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	binary_tree_t **arr;
-	binary_tree_t *tmp;
-	int start = 0, size, index = start, flag = 0;
-
-	size = binary_tree_size(tree);
-	arr = malloc(sizeof(binary_tree_t *) * size);
-	if (!arr)
+	if (!tree)
 		return (0);
-	arr[start] = (binary_tree_t *)tree;
-	index++;
-	while (start < size)
-	{
-		tmp = arr[start];
-		if ((flag && (tmp->left || tmp->right)) || (tmp->right && !tmp->left))
-		{
-			free(arr);
-			return (0);
-		}
-		start++;
-		if (tmp->left)
-		{
-			arr[index] = tmp->left;
-			index++;
-		}
-		else
-			flag = 1;
-		if (tmp->right)
-		{
-			arr[index] = tmp->right;
-			index++;
-		}
-		else
-			flag = 1;
-	}
-	return (1);
-	free(arr);
+	return (binary_tree_is_complete_help(tree, 0, binary_tree_size(tree)));
 }
