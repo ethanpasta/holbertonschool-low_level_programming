537f6b43af951819c11ecff8f1c211773e657746
ethanpasta <ethan.mayer9@gmail.com>
2019-07-25 14:09:12 +0000

    Conversion 'unsigned int

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -47,7 +47,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	height = binary_tree_height(tree);
 	size = binary_tree_size(tree);
-	if ((unsigned)(1 << height) - 1 == size)
+	if ((unsigned int)(1 << height) - 1 == size)
 		return (1);
 	return (0);
 }
